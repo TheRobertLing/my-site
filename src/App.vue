660109b3b5b41929273a920ef8ad54e0d2ebff2c
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-09 00:42:18 +1100

    defined page structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeroSection from './components/HeroSection.vue';
+import AboutSection from './components/AboutSection.vue';
+import ProjectsSection from './components/ProjectsSection.vue';
+import WorkSection from './components/WorkSection.vue';
+import ContactSection from './components/ContactSection.vue';
+import FooterSection from './components/FooterSection.vue';
+</script>
 
 <template>
-  hi
+  <HeroSection/>
+  <AboutSection/>
+  <ProjectsSection/>
+  <WorkSection/>
+  <ContactSection/>
+  <FooterSection/>
 </template>
 
 <style scoped></style>
