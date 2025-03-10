94e7417a44eecaf867d2fbde862d78d40b2aa94f
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-10 23:52:04 +1100

    added work section

---
+++ src/components/WorkSection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="work" class="w-9/10 mx-auto">
+    <h2 class="mb-5 text-5xl tracking-tight font-bold">Work and Extracurriculars</h2>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
