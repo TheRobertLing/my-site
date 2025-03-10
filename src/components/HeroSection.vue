91a3a3f96e3bf8fce70b52080f42fc5dfd027539
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-10 23:47:25 +1100

    added hero section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="hero min-h-screen">
-    <div class="hero-overlay">
-      <canvas ref="canvas">
-        
-
-      </canvas>
-    </div>
+  <div id="hero" class="hero min-h-screen">
+    <!-- <div class="hero-overlay">
+      <canvas ref="canvas"> </canvas>
+    </div> -->
     <div class="hero-content text-neutral-content text-center">
-      <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hey there, I'm Leo</h1>
-        <p class="mb-5">Welcome to my site.</p>
-        <button class="btn btn-primary rounded-xl border-2 border-solid hover:border-[var(--color-neutral-content)]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            class="fill-current"
-          >
-            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
-          </svg>
+      <div class="max-w-2xl">
+        <h1 class="mb-5 text-7xl font-bold" ref="main">Hey there, I'm <span class="text-green-500">Leo</span>.</h1>
+        <p class="mb-5 text-2xl" ref="second">Welcome to my Site.</p>
+        <button
+          class="btn btn-primary rounded-xl border-2 border-solid hover:border-[var(--color-green-500)]"
+        >
+          <a href="#about">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              class="fill-current"
+            >
+              <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+            </svg>
+          </a>
         </button>
       </div>
     </div>
